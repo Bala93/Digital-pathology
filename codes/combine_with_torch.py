5d1f38b5bfc731a53247f9065870dc1ce3898298
Bala93 <balamuralim.1993@gmail.com>
2018-06-04 19:05:58 +0530

    torch and sample images

--- codes/combine_with_torch.py
+++ codes/combine_with_torch.py
@@ -6,13 +6,26 @@ import numpy as np
 from tqdm import tqdm
 import argparse
 import glob
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+from PIL import Image
+
 
 if __name__ == "__main__":
 
     # This is for obtaining square images
     # TODO: For non-square images implement, rewrite print statement in run 
     #python create_patch.py --input_path='/media/balamurali/Seagate Backup Plus Drive/Mitosis_detection/2012_aperio_scan_image/whole_images' --output_path='/media/balamurali/Seagate Backup Plus Drive/Mitosis_detection/2012_aperio_scan_image/sampled_images' --img_ext='bmp' --stride=32 --img_size=256 --img_size=512
-    #python create_patch.py --input_path='/media/htic/Seagate Backup Plus Drive/Mitosis_detection/2012_aperio_scan_image/mitosis_evaluation_mask' --output_path='/media/htic/Seagate Backup Plus Drive/Mitosis_detection/2012_aperio_scan_image/dataset/test_data/' --img_ext='bmp' --stride=512 --img_size=512
+    #python create_patch.py --input_path='/media/htic/NewVolume1/murali/mitosis/dataset/eval/' --img_ext='png' --stride=512 --img_size=512
 
 
     parser = argparse.ArgumentParser('Sample images giving image size and stride -- Please give absolute path')
@@ -22,11 +35,11 @@ if __name__ == "__main__":
         type = str,
         help = 'provide path which contains images' )
 
-    parser.add_argument(
-        '--output_path',
-        required = True,
-        type = str,
-        help = 'provide path to save the images')
+    # parser.add_argument(
+    #     '--output_path',
+    #     # required = True,
+    #     type = str,
+    #     help = 'provide path to save the images')
 
     parser.add_argument(
         '--img_ext',
@@ -53,7 +66,7 @@ if __name__ == "__main__":
     opt = parser.parse_args()    
     img_folder = opt.input_path
     # img_folder = '/media/balamurali/NewVolume2/Deep_network/mitosis'
-    out_folder = opt.output_path
+    # out_folder = opt.output_path
     # out_folder = '/media/balamurali/NewVolume2/Deep_network/mitosis/output'
     img_ext    = opt.img_ext
     img_size   = opt.img_size
@@ -68,28 +81,52 @@ if __name__ == "__main__":
         stride_list.append(each)
 
 
-    print ("Settings: \nInput Folder:{}\nOutput Folder:{}\nImg ext:{}\nImg size:{}\n".format(
-        img_folder,out_folder,img_ext,window_shape_list))
+    model_path  = '/media/htic/NewVolume1/murali/mitosis/weight/whole_slide.pt'
+    model_ft = models.resnet101()
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs,2)
+    model_ft.load_state_dict(torch.load(model_path))
+    model_ft.cuda()
+
+    data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomSizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Scale(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    }
+
+
+    # print ("Settings: \nInput Folder:{}\nOutput Folder:{}\nImg ext:{}\nImg size:{}\n".format(
+        # img_folder,out_folder,img_ext,window_shape_list))
 
 
     imgs_path  = glob.glob(os.path.join(img_folder,'*.' + img_ext))    
+
     for window_shape in tqdm(window_shape_list):
         print ("Window size : {}\n".format(window_shape))
         
         for stride in tqdm(stride_list):
             print ("Stride size : {}\n".format(stride))
                 
-            out_folder_temp = os.path.join(out_folder,'size_' + str(window_shape[0]) + '_' + 'stride_' +str(stride))    
+            # out_folder_temp = os.path.join(out_folder,'size_' + str(window_shape[0]) + '_' + 'stride_' +str(stride))    
         
-            if not os.path.exists(out_folder_temp):
-                print ("Directory {} not found,creating one".format(out_folder_temp))
-                os.mkdir(out_folder_temp)
+            # if not os.path.exists(out_folder_temp):
+            #     print ("Directory {} not found,creating one".format(out_folder_temp))
+            #     os.mkdir(out_folder_temp)
         
             print ("Sampling images\n")
             for img_path in tqdm(imgs_path):
                 # Read image
                 img = io.imread(img_path)
-                print (len(img.shape))
+                # print (len(img.shape))
                 if len(img.shape) == 3:
                     r_channel = img[:,:,0]
                     g_channel = img[:,:,1]
@@ -113,6 +150,33 @@ if __name__ == "__main__":
                             img_sample = np.dstack((sample_r,sample_g,sample_b))
                         else:
                             img_sample = r_sample[row,col]  
+
+                        # img_sample = np.swapaxes(img_sample,0,1)
+                        # img_sample = np.swapaxes(img_sample,0,2)
+                        # img_sample = torch.FloatTensor(img_sample)
+                        # img_sample = img_sample.unsqueeze(0)
+                        print (img_sample.shape)
+                        img_sample = Image.fromarray(img_sample)
+                        img_sample = data_transforms['val'](img_sample)
+                        print (img_sample.shape )
+
+                        break
+                    break
+                break
+            break
+        break
+                            # inputs = Variable(img_s.cuda())
+
+                        # outputs = model_ft(inputs)
+                        # _, preds = torch.max(outputs.data, 1)
+
+                        # y_true.append(labels.data.cpu().numpy()) 
+                        # y_pred.append(preds.cpu().numpy())
+
+                        # print (y_pred[0][0],y_true[0][0])
+
+
+
                         # print (img_sample.shape)
                         # img_sample_path = (os.path.join(out_folder_temp,os.path.basename(img_path)[:-4] + '_{}_{}.jpg')).format(row,col)
                         # io.imsave(img_sample_path,img_sample)
@@ -120,3 +184,13 @@ if __name__ == "__main__":
                     # break
                 # break
             # break
+
+# use_gpu = torch.cuda.is_available()
+# y_true = []
+# y_pred = []
+
+
+
+
+# plt.imshow(inputs.cpu())
+# break
