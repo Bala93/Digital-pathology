8182038441f0f2a23c31d94298de261cc6d1d7b1
Bala93 <balamuralim.1993@gmail.com>
2018-06-05 16:17:52 +0530

    Removed 2 in text

--- codes/create_json_xml.py
+++ codes/create_json_xml.py
@@ -6,8 +6,8 @@ from tqdm import tqdm
 
 if __name__ == "__main__":
 
-    xml_path = '/media/balamurali/NewVolume2/IIT-HTIC/GE_Project/Dataset/Tensorflow/annotations/xmls/'
-    json_path = '/media/balamurali/NewVolume2/IIT-HTIC/GE_Project/Dataset/Tensorflow/annotations/xmls/out.json'
+    xml_path = '/media/htic/Seagate Backup Plus Drive/mitotis/FasterRCNN_evaluation/xml_mitosis/' 
+    json_path = '/media/htic/NewVolume1/murali/mitosis/dataset/mitotic_count/results/original.json'
     xml_files = glob.glob(xml_path + '*.xml')
     
     metrics_gt = {}
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         # xml_file = os.path.join(xml_path,'1_3000.xml')
         tree = ET.parse(xml_file)
         root = tree.getroot()
-        filename = root.find('filename').text2
+        filename = root.find('filename').text
         bndbox_list = []
 
         for bndbox in root.iter('bndbox'):
